67351c68b417ecfbee61d3578fe5a29e3b0940ce
dev-raimundos <devraimundos@outlook.com>
2026-05-27 14:53:16 -0300

    Feat: adiciona extensão de interceptador de exceções

---
+++ App/Core/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,12 @@
+﻿using NeonVertexApi.App.Core.Middleware;
+
+namespace NeonVertexApi.App.Core.Extensions;
+
+public static class WebApplicationExtensions
+{
+    public static WebApplication UseCore(this WebApplication app)
+    {
+        app.UseMiddleware<ExceptionMiddleware>();
+        return app;
+    }
+}
