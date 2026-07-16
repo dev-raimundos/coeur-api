eac850ecf048ea8e36e5448cc0d1e05a2633735d
dev-raimundos <devraimundos@outlook.com>
2026-07-16 08:27:47 -0300

    fix: adiciona chaves de acesso

--- src/modules/Users/Application/Abstractions/IUsersRepository.cs
+++ src/modules/Users/Application/Abstractions/IUsersRepository.cs
@@ -4,9 +4,9 @@ namespace CoeurApi.Modules.Users.Application.Abstractions;
 
 public interface IUsersRepository
 {
-    Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
-    Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
-    Task<bool> ExistsByEmailAsync(string email, CancellationToken cancellationToken = default);
-    Task AddAsync(User user, CancellationToken cancellationToken = default);
-    void Delete(User user);
+    public Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    public Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
+    public Task<bool> ExistsByEmailAsync(string email, CancellationToken cancellationToken = default);
+    public Task AddAsync(User user, CancellationToken cancellationToken = default);
+    public void Delete(User user);
 }
